6acc2f6aefa3f6898e507a25e7d8aa486c3c8442
yujjinn <35555129+yujjinn@users.noreply.github.com>
2020-07-02 14:55:02 +0900

    Create 10809.java

---
+++ 10809.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+        
+        for (char c = 'a' ; c <= 'z' ; c++)
+            System.out.print(s.indexOf(c) + " ");
+    }	
+}
