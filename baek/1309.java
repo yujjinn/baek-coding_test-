0f09cd380e62f8becc1c4655023e8f29f3febd67
yujjinn <35555129+yujjinn@users.noreply.github.com>
2020-07-21 20:38:59 +0900

    Create 1309.java

---
+++ baek/1309.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);	
+		int n = sc.nextInt();
+		int dp[] = new int[100001];
+		dp[0]=1;
+		dp[1]=3;
+		dp[2]=7;
+		
+		for(int i=3; i<=n; i++) {
+			dp[i]=dp[i-1]*2+dp[i-2];
+			dp[i] %= 9901;
+		}
+		System.out.println(dp[n]);
+	}
+	
+
+}
