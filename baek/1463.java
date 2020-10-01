cf50940d77d5bbb0a1e8dba865ec35b1833283f8
yujjinn <35555129+yujjinn@users.noreply.github.com>
2020-10-01 18:20:33 +0900

    Create 1463.java

---
+++ baek/1463.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+	static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int dp [] = new int[n+1];
+    	
+    	for(int i=2; i<=n; i++) {
+    		dp[i]=dp[i-1]+1;
+    		if(i%2==0) {dp[i]=Math.min(dp[i], dp[i/2]+1);}
+    		if(i%3==0) {dp[i]=Math.min(dp[i], dp[i/3]+1);}
+    	}
+    	System.out.println(dp[n]);
+    }
+
+}
+​
